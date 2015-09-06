17742e6a510d3288bbaf8a1ffda5e177efab296a
Sabina Linder <sl222mq@student.lnu.se>
2015-09-06 15:53:22 +0200

    DateTimeView implemented

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -5,7 +5,8 @@ class DateTimeView {
 
 	public function show() {
 
-		$timeString = 'TODO, Write servertime here...';
+		$date = new DateTime();
+		$timeString = $date->format("l").", the ".$date->format("jS")." of ".$date->format("F Y"). ", the time is ".$date->format("H:i:s"); 
 
 		return '<p>' . $timeString . '</p>';
 	}
